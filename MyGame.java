db2edf461fda828d923c8e3052356806bf2bda9e
SID-Suryawanshi <siddhantsurywanshi99@gmail.com>
2025-09-29 02:13:41 +0530

    Initial commit: Add Tic-Tac-Toe game

---
+++ MyGame.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+public class MyGame {
+    public static void createBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = '.';
+            }
+        }
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean checkWinner(char[][] board, char player) {
+        for (int i = 0; i < board.length; i++) {
+            // rows:
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+            // Columns:
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                return true;
+            }
+            if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+                return true;
+            }
+            if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean drawGame(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        final int BOARD_WIDTH = 3;
+        final int BOARD_HEIGHT = 3;
+        char[][] gameBoard = new char[BOARD_HEIGHT][BOARD_WIDTH];
+
+        char playerX = 'X';
+        char playerO = 'O';
+
+        createBoard(gameBoard);
+
+        char currentPlayer = playerX;
+
+        printBoard(gameBoard);
+        while (true) {
+
+            System.out.println("currentPlayer : " + currentPlayer);
+
+            System.out.print("Enter row (0-2) : ");
+            int row = sc.nextInt();
+            System.out.print("Enter column (0-2) : ");
+            int col = sc.nextInt();
+            System.out.println();
+
+            if (row < 0 || row > gameBoard.length || col < 0 || col > gameBoard[0].length) {
+                System.out.println("Invalid coordinates!");
+                continue;
+            }
+
+            // Block player from overriding the playerPos:
+            boolean currPos = true;
+            if (gameBoard[row][col] == '.' && currPos) {
+                gameBoard[row][col] = currentPlayer;
+            } else {
+                currPos = false;
+            }
+
+            if (checkWinner(gameBoard, currentPlayer)) {
+                printBoard(gameBoard);
+                System.out.println("Winner is " + currentPlayer);
+                break;
+            } else if (drawGame(gameBoard)) {
+                printBoard(gameBoard);
+                System.out.println("It's a Draw!");
+                break;
+            }
+
+            printBoard(gameBoard);
+
+            // player Switch:
+            if (currentPlayer == playerX) {
+                currentPlayer = playerO;
+            } else {
+                currentPlayer = playerX;
+            }
+
+        }
+        System.out.println("Game Over. Thanks for playing!");
+        sc.close();
+    }
+}
